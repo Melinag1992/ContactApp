b9d8234d668914b2802924ca7924c4e7ea0e3d0c
Melina Gonzalez <melinagonzalez@Melinas-MBP.cable.rcn.com>
2018-11-05 11:21:40 -0500

    recycler view displays data from API

--- app/src/main/java/com/example/melinagonzalez/genius_plaza_melinag/view/UserViewHolder.java
+++ app/src/main/java/com/example/melinagonzalez/genius_plaza_melinag/view/UserViewHolder.java
@@ -1,28 +1,48 @@
 package com.example.melinagonzalez.genius_plaza_melinag.view;
 
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+
 import android.view.View;
+import android.widget.TextView;
 
 
+import com.bumptech.glide.Glide;
+import com.example.melinagonzalez.genius_plaza_melinag.R;
 import com.example.melinagonzalez.genius_plaza_melinag.model.Data;
 
 
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
 public class UserViewHolder extends RecyclerView.ViewHolder {
-    private String TAG;
+    private CircleImageView imageview;
+    private TextView lastNameTV;
+    private TextView firstNameTV;
+
+
 
     public UserViewHolder(View itemView) {
         super(itemView);
-        
+        setViews(itemView);
         
     }
 
 
     public void onBind(Data personData){
+        firstNameTV.setText(personData.getFirst_name());
+        lastNameTV.setText(personData.getLast_name());
+        Glide.with(itemView.getContext())
+                .load(personData.getAvatar())
+                .into(imageview);
 
 
-        Log.d(TAG, "onBind: " + personData.getFirst_name());
+    }
+
+    public void setViews(View itemView){
+        firstNameTV = itemView.findViewById(R.id.first_name);
+        lastNameTV =  itemView.findViewById(R.id.last_name);
+        imageview = itemView.findViewById(R.id.avatar_imageview);
 
     }
 }
